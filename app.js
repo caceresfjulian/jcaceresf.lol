0705a6be2ab4b2fb443b0393f70c2754eb4f34c9
caceresfjulian <caceresf.julian@gmail.com>
2023-07-29 18:20:41 +0000

    Create a basic websocket connection

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-const http = require('http');
+const express = require('express');
+const server = require('http').createServer();
+const app = express();
 
-http.createServer((req, res) => {
-	res.write('On the way to being a full stack engineer!');
-	res.end();
-}).listen(3000);
+app.get('/', function (req, res) {
+	res.sendFile('index.html', {root: __dirname});
+})
 
-console.log('Server started at port 3000');
+server.on('request', app);
+server.listen(3000, function () {
+		console.log('Listening on port 3000');
+});
+
+/** Websocket **/
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({server: server});
+
+wss.on('connection', function (ws) {
+	const numClients = wss.clients.size;
+	console.log('Clients connected ', numClients);
+
+	wss.broadcast(`Current visitors ${numClients}`);
+
+	if(ws.readyState === ws.OPEN) {
+		ws.send('Welcome to my server');
+	}
+
+	ws.on('close', function close() {
+		wss.broadcast(`Current visitors: ${wss.clients.size}`);
+		console.log('A client has disconnected');
+	})
+});
+
+wss.broadcast = function broadcast(data) {
+	wss.clients.forEach(function each(client){
+		client.send(data);
+	})
+};
